6060e2267b80f83ea8aa228bd43bdbd4dd36637d
Jason Bland <jtbland@gmail.com>
2019-03-19 23:18:18 -0700

    add create event route

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 require('./models/User');
 require('./models/Event');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);
+mongoose
+  .connect(keys.mongoURI)
+  .then(() => 'You are connected to Mongo!')
+  .catch(err => console.error('Something went wrong', err));
 
 const app = express();
+
+app.use(bodyParser.json());
 
 require('./routes/eventsRoutes')(app);
 
--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -2,4 +2,14 @@ module.exports = app => {
   app.get('/', (req, res) => {
     res.send('Enter an event');
   });
+
+  app.post('/api/create_event', (req, res) => {
+    const { name } = req.body;
+
+    if (name) {
+      res.status(200).send(name);
+    } else {
+      res.status(422).send('Event not created!');
+    }
+  });
 };
